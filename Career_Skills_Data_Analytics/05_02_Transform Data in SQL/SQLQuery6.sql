e2c2e6f4d5866e4a821b0e57c066e93661afeeef
LYC <90447099+lc4695@users.noreply.github.com>
2023-07-05 12:19:30 -0400

    Delete Career_Skills_Data_Analytics directory

--- Career_Skills_Data_Analytics/05_02_Transform Data in SQL/SQLQuery6.sql
+++
@@ -1,2 +0,0 @@
-SELECT     SupplierCategoryID, SupplierCategoryName, LastEditedBy
-FROM        Purchasing.SupplierCategories
--- Career_Skills_Data_Analytics/05_02_Transform Data in SQL/SQLStatementforSupplierTransactions.sql
+++
@@ -1,4 +0,0 @@
-SELECT     SupplierTransactionID, SupplierID, PurchaseOrderID, SupplierInvoiceNumber, TransactionDate, AmountExcludingTax, TaxAmount, FinalizationDate
-FROM        Purchasing.SupplierTransactions
-WHere FinalizationDate > '2014-12-31'
-Order By SupplierID, FinalizationDate
